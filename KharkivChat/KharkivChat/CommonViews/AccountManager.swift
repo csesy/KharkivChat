77aab29a5e1af440d86a7c5fe8c917661ff86d81
Ievgen Naloiko <ievgen.naloiko@searchconcepts.ch>
2016-12-23 19:43:53 +0200

    First view controller

---
+++ KharkivChat/KharkivChat/CommonViews/AccountManager.swift
@@ -0,0 +1,9 @@
+//
+//  AccountManager.swift
+//  KharkivChat
+//
+//  Created by Ievgen Naloiko on 12/23/16.
+//  Copyright © 2016 Naloiko. All rights reserved.
+//
+
+import Foundation
---
+++ KharkivChat/KharkivChat/CommonViews/insetTextFild.swift
@@ -0,0 +1,9 @@
+//
+//  insetTextFild.swift
+//  KharkivChat
+//
+//  Created by Ievgen Naloiko on 12/23/16.
+//  Copyright © 2016 Naloiko. All rights reserved.
+//
+
+import Foundation

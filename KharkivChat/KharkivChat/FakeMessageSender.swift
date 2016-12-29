24ec335849bb0f5e5759f5ed9985929d31fb6666
Eugene Naloiko <naloiko@gmail.com>
2016-12-29 15:53:57 +0200

    Code clean up.

--- KharkivChat/KharkivChat/FakeMessageSender.swift
+++ KharkivChat/KharkivChat/FakeMessageSender.swift
@@ -37,7 +37,6 @@ public class FakeMessageSender {
     public func sendMessages(_ messages: [DemoMessageModelProtocol]) {
         for message in messages {
             self.sendMessage(message)
-            //self.fakeMessageStatus(message)
         }
     }
 
@@ -55,36 +54,7 @@ public class FakeMessageSender {
                 self.updateMessage(message, status: .failed)
             }
         }
-        
-        //self.fakeMessageStatus(message)
     }
-
-//    private func fakeMessageStatus(_ message: DemoMessageModelProtocol) {
-//        switch message.status {
-//        case .success:
-//            break
-//        case .failed:
-////            self.updateMessage(message, status: .sending)
-////            self.fakeMessageStatus(message)
-//            break
-//        case .sending:
-////            switch arc4random_uniform(100) % 5 {
-////            case 0:
-////                if arc4random_uniform(100) % 2 == 0 {
-////                    self.updateMessage(message, status: .failed)
-////                } else {
-////                    self.updateMessage(message, status: .success)
-////                }
-////            default:
-////                let delaySeconds: Double = Double(arc4random_uniform(1200)) / 1000.0
-////                let delayTime = DispatchTime.now() + Double(Int64(delaySeconds * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-////                DispatchQueue.main.asyncAfter(deadline: delayTime) {
-////                    self.fakeMessageStatus(message)
-////                }
-////            }
-//            break
-//        }
-//    }
 
     private func updateMessage(_ message: DemoMessageModelProtocol, status: MessageStatus) {
         if message.status != status {
